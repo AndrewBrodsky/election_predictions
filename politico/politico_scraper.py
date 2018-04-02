2a8a3804b47dbfb15f8d3f6dea9cc24f89fb50d7
Andrew Brodsky <andrew@brodskyresearch.com>
2018-04-01 19:40:26 -0600

    added dark money data

--- politico/politico_scraper.py
+++ politico/politico_scraper.py
@@ -201,4 +201,4 @@ if __name__ == "__main__":
     }
 
     votes_df['state_abbr'] =votes_df['state'].map(state_dict)
-    votes_df['LASTNAME'] = votes_df.cand_name.str.upper().str.split(" ").str[-1]
+    votes_df['LAST_NAME'] = votes_df.cand_name.str.upper().str.split(" ").str[-1]
