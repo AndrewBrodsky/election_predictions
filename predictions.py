268598228913be93b1f572b80f00938bcfe2a9c5
Andrew Brodsky <andrew@brodskyresearch.com>
2018-04-03 15:25:50 -0600

    Got data in shape to ran models. Not good models, but still.

--- predictions.py
+++ predictions.py
@@ -48,6 +48,26 @@ def join_files(politico, fec, acs, dark_house):
     return alldata3
 
 
+def make_model_data(dataframe):
+
+    model_data = dataframe.filter(items = ['YEAR2016', 'DEM', 'incumbent', 'TRANS_BY_INDIV', 'TRANS_BY_CMTE',
+       'DARK_FOR', 'DARK_AGAINST', 'B01003_001E', 'B01002_001E', 'B02001_002E',
+       'B02001_003E', 'B02001_004E', 'B02001_005E', 'B02001_006E', 'B02001_008E',
+       'B05001_006E', 'B05001_005E', 'B05002_003E', 'B05012_001E',
+       'B06007_001E', 'B06007_002E', 'B06007_003E', 'B06009_002E',
+       'B06009_003E', 'B06009_004E', 'B06009_011E', 'B06009_012E',
+       'B06010_001E', 'B06010_002E', 'B06011_001E', 'B06012_001E',
+       'B06012_002E', 'B06012_003E', 'vote_count'])
+
+
+    model_data['TRANS_BY_INDIV'].fillna(0, inplace = True)
+    model_data['TRANS_BY_CMTE'].fillna(0, inplace = True)
+    model_data['DARK_FOR'].fillna(0, inplace = True)
+    model_data['DARK_AGAINST'].fillna(0, inplace = True)
+    model_data = model_data.dropna(axis=0, how='any')
+
+    return model_data
+
 
 if __name__ == "__main__":
 
@@ -62,33 +82,25 @@ if __name__ == "__main__":
     if afterpickle == True:
         alldata = pickle.load( open( "save.p", "rb" ) )
 
-        features = ['YEAR2016', 'DEM', 'incumbent', 'TRANS_BY_INDIV', 'TRANS_BY_CMTE',
-           'DARK_FOR', 'DARK_AGAINST', 'B01003_001E', 'B01002_001E', 'B02001_002E',
-           'B02001_003E', 'B02001_004E', 'B02001_005E', 'B02001_006E', 'B02001_008E',
-           'B05001_006E', 'B05001_005E', 'B05002_003E', 'B05012_001E',
-           'B06007_001E', 'B06007_002E', 'B06007_003E', 'B06009_002E',
-           'B06009_003E', 'B06009_004E', 'B06009_011E', 'B06009_012E',
-           'B06010_001E', 'B06010_002E', 'B06011_001E', 'B06012_001E',
-           'B06012_002E', 'B06012_003E']
+        make_model_data(alldata)
 
-        X = alldata[features]
-        X['TRANS_BY_INDIV'].fillna(0, inplace = True)
-        X['TRANS_BY_CMTE'].fillna(0, inplace = True)
-        X['DARK_FOR'].fillna(0, inplace = True)
-        X['DARK_AGAINST'].fillna(0, inplace=True)
-
-
-        y = alldata['vote_count']
+        X = model_data.drop('vote_count', axis=1)
+        y = model_data['vote_count']
         X_train, X_test, y_train, y_test = train_test_split(X,y)
 
-        Linear_model = LinearRegression()
+        linear_model = LinearRegression()
         RF_model = RandomForestRegressor()
         GBR_model = GradientBoostingRegressor()
         MLP_model = MLPRegressor()
 
-        Linear_model.fit(X_train, y_train)
-        Linear_model.predict(X_test)
+        linear_model.fit(X_train, y_train)
+        lin_pred = Linear_model.predict(X_test)
 
-        # pipeline = make_pipeline (Linear_model, RF_model)
-        # pipeline.fit(X_train, y_train)
-        # y_pred = pipeline.predict_proba(X_test)
+        RF_model.fit(X_train, y_train)
+        RF_model.score
+
+        GBR_model.fit(X_train, y_train)
+
+        pipeline = make_pipeline (Linear_model, RF_model)
+        pipeline.fit(X_train, y_train)
+        thescore = pipeline.score(X_test)
